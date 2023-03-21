92da502d1e66a2ba17027dc0708dc04139436aa7
Shawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2023-03-21 08:21:54 -0700

    Update Mario AI - Preprocess.py

--- Open AI Gym/Mario AI/Mario AI - Preprocess.py
+++ Open AI Gym/Mario AI/Mario AI - Preprocess.py
@@ -1,1 +1,6 @@
-
+# Import Frame Stacker Wrapper and GrayScale Wrapper
+from gym.wrappers import FrameStack, GrayScale Observation
+# Import Vectorization Wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+# Import Matplotlib
+from matplotlib import pyplot as plt
